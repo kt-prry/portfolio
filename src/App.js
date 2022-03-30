ca7e3bec0f220ace82060817d0806d752d70d3f2
Kati <katherine.f235@gmail.com>
2022-03-29 22:18:55 -0400

    Configure react app for gh-pages deployment

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import styled from 'styled-components'
+import './App.css';
+
+const Text = styled.p`
+  color: red;
+`
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Text>
+          Edit <code>src/App.js</code> and save to reload.
+        </Text>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
